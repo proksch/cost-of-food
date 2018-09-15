4c787cb3ef01fc5b71965fb197dd65328b564145
Sebastian Proksch <development@mail.proks.ch>
2018-09-15 14:36:05 +0200

    First Integration into Instagram

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ chrome.runtime.onInstalled.addListener(function() {
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "developer.chrome.com" }
+            pageUrl: { hostEquals: "www.instagram.com" }
           })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
--- content-script.js
+++ content-script.js
@@ -1,1 +1,23 @@
 console.log("content script loaded!")
+
+var btn = document.createElement("a");
+btn.appendChild(document.createTextNode("$$$")); 
+btn.id = "cof-button"
+btn.href="#"
+
+btn.onclick = function() {
+    alert("hey!")
+}
+
+var section = document.getElementsByClassName("ltpMr Slqrh")[0]
+section.appendChild(btn)
+
+modal = document.createElement("div")
+modal.id = "cof-container"
+modal.appendChild(document.createTextNode("asd"))
+
+bg = document.createElement("div")
+bg.id = "cof-background"
+bg.appendChild(modal)
+
+document.body.appendChild(bg)
